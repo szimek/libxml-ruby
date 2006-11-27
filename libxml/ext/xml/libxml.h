e3bf3892cba78bc4ac0e3adba4f21898dc85db8c
roscopeco <roscopeco@3c02cb77-7c1c-4cee-9c41-00a59c9a4278>
2006-11-27 10:22:36 +0000

    Updated to development 0.3.8.3 Added patch version macro Added patch version handling to Rakefile release tasks

--- libxml/ext/xml/libxml.h
+++ libxml/ext/xml/libxml.h
@@ -6,11 +6,12 @@
 /* Don't nuke this block!  It is used for automatically updating the
  * versions below. VERSION = string formatting, VERNUM = numbered
  * version for inline testing: increment both or none at all. */
-#define RUBY_LIBXML_VERSION  "0.3.8.2"
-#define RUBY_LIBXML_VERNUM   382
+#define RUBY_LIBXML_VERSION  "0.3.8.3"
+#define RUBY_LIBXML_VERNUM   383
 #define RUBY_LIBXML_VER_MAJ   0
 #define RUBY_LIBXML_VER_MIN   3
 #define RUBY_LIBXML_VER_MIC   8
+#define RUBY_LIBXML_VER_PATCH 3
 
 #include <ruby.h>
 #include <rubyio.h>
